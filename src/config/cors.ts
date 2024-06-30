6e6667dae0528ef8137e9c871666fd0b34a2b58b
BryanVas24 <bryanvasquez2406@gmail.com>
2024-06-30 16:40:25 -0600

    solucionando problema de cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,6 +3,11 @@ import { CorsOptions } from "cors";
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
     const whiteList = [process.env.FRONTEND_URL];
+    //esto es para permitir que thunderclient haga solicitudes
+    //sale del .json
+    if (process.argv[2] === "--api") {
+      whiteList.push(undefined);
+    }
     if (whiteList.includes(origin)) {
       callback(null, true);
     } else {
