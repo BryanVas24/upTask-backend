9631009a85fbaf6a7e0c79b00c480514c6c8812b
BryanVas24 <bryanvasquez2406@gmail.com>
2024-07-01 12:27:39 -0600

    Creando el modelo del token

---
+++ src/models/Token.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+export interface IToken extends Document {
+  token: string;
+  user: Types.ObjectId;
+  created: string;
+}
+
+const tokenSchema: Schema = new Schema({
+  token: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: Types.ObjectId,
+    ref: "User",
+  },
+  created: {
+    type: Date,
+    default: Date.now(),
+    //esto es el tiempo de duración del token, acá dura un dia
+    expires: "1d",
+    required: true,
+  },
+});
+
+const token = mongoose.model<IToken>("Token", tokenSchema);
+export default token;
