ec48375316092d728429b8e3c2f605cb02214802
BryanVas24 <bryanvasquez2406@gmail.com>
2024-06-30 23:53:58 -0600

    Previniendo usuarios duplicados

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,11 +4,18 @@ import bcrypt from "bcrypt";
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
+      const { email, password } = req.body;
+      //revisa si el usuario ya esta registrado
+      const userExist = await User.findOne({ email });
+      if (userExist) {
+        const error = new Error("El usuario ya esta registrado");
+        return res.status(409).json({ error: error.message });
+      }
       const user = new User(req.body);
       //hash del password
       //el numero del salt es que tan codificada va a estar, entre mas alto el numero mas recursos consume, 10 es algo recomendado
       const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(req.body.password, salt);
+      user.password = await bcrypt.hash(password, salt);
       await user.save();
       res.send("Cuenta creada, revisa tu email para confirmarla");
     } catch (error) {
