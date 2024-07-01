c0daa922cb062ef06691eb9b64a83c482deb2034
BryanVas24 <bryanvasquez2406@gmail.com>
2024-06-30 23:44:44 -0600

    agregando dependencia bcrypt y encriptando password

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from "express";
 import User from "../models/User";
+import bcrypt from "bcrypt";
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
       const user = new User(req.body);
+      //hash del password
+      //el numero del salt es que tan codificada va a estar, entre mas alto el numero mas recursos consume, 10 es algo recomendado
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(req.body.password, salt);
       await user.save();
       res.send("Cuenta creada, revisa tu email para confirmarla");
     } catch (error) {
