d0fb8f15344e00a53fc5d230415887f73f6b703a
BryanVas24 <bryanvasquez2406@gmail.com>
2024-06-30 23:21:17 -0600

    Creando algoritmo para creación de cuenta

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from "express";
+import User from "../models/User";
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
-    res.send("desde el creador de cuentas");
+    try {
+      const user = new User(req.body);
+      await user.save();
+      res.send("Cuenta creada, revisa tu email para confirmarla");
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un error" });
+    }
   };
 }
